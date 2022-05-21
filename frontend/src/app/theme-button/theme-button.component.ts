7f66977819a3556c0c2412596b10a3fcaf81f71f
palo-landrae <louiandraepalo@gmail.com>
2022-05-21 08:15:10 +0000

    :sparkles: change theme logic

--- frontend/src/app/theme-button/theme-button.component.ts
+++ frontend/src/app/theme-button/theme-button.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-theme-button',
   templateUrl: './theme-button.component.html',
-  styleUrls: ['./theme-button.component.css']
+  styleUrls: ['./theme-button.component.css'],
 })
 export class ThemeButtonComponent implements OnInit {
+  isDarkEnable = false;
+  currentTheme$ = new BehaviorSubject<string>('light');
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
+  changeTheme() {
+    this.renderer.removeClass(document.body, this.currentTheme$.value);
+    this.currentTheme$.value === 'light'
+      ? this.currentTheme$.next('dark')
+      : this.currentTheme$.next('light');
+    this.renderer.addClass(document.body, this.currentTheme$.value);
+    localStorage.setItem('theme', this.currentTheme$.value);
+    this.isDarkEnable = !this.isDarkEnable;
   }
 
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.currentTheme$.next(savedTheme);
+    }
+    this.renderer.addClass(document.body, this.currentTheme$.value);
+  }
 }
